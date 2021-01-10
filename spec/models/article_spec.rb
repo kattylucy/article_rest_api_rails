219976d963a8228b962fdb1eddb5805cd8afcb0f
kattylucy <kattybarroso1321@gmail.com>
2021-01-10 12:08:38 -0800

    added article validation for title, content and slug

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#validations' do
+
+    it "should test that the factory is valid" do
+        expect(build :article).to be_valid
+    end
+
+    it "should validate the presence of the title" do
+        article = build :article, title: ''
+        expect(article).not_to be_valid
+        expect(article.errors.messages[:title]).to include("can't be blank")    
+    end
+
+    it "should validate the presence of the content" do
+        article = build :article, content: ''
+        expect(article).not_to be_valid
+        expect(article.errors.messages[:content]).to include("can't be blank")    
+    end
+
+    # it "should validate the presence of the slug" do
+    #   article = FactoryBot.build :article, slug: ''
+    #   expect(article).not_to be_valid
+    #   expect(article.errors.messages[:slug]).to include("cant be blank")    
+    # end
+
+    # it "should validate the uniqueness of the slug" do
+    #   article = FactoryBot.build :article, content: ''
+    #   invalid_article
+      
+    # end
+
+  end
 end
