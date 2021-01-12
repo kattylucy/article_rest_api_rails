937bd463dfba55768cd619fcc5fc902c106b18d1
kattylucy <kattybarroso1321@gmail.com>
2021-01-12 13:37:31 -0800

    refactored the perform method to make it clearner

--- app/lib/user_authenticator.rb
+++ app/lib/user_authenticator.rb
@@ -8,24 +8,41 @@ class UserAuthenticator
     end
 
     def perform
-        client = Octokit::Client.new(
-            client_id: Figaro.env.super_secret_github_id,
-            client_secret: Figaro.env.super_secret_github_key
-        )
-        response = client.exchange_code_for_token(code)
         if response.try(:error).present?
             raise AuthenticationError
         else
-            user_client = Octokit::Client.new( access_token: response )
-            user_data = user_client.user.to_h.slice(
-                :login, :avatar_url, :url, :name
-            )
-            User.create(user_data.merge(provider: 'github'))
+            prepare_user
         end
     end
 
 
     private
+
+    def client
+        @client ||= Octokit::Client.new(
+            client_id: Figaro.env.super_secret_github_id,
+            client_secret: Figaro.env.super_secret_github_key
+        )
+    end
+
+    def response
+        @response ||= client.exchange_code_for_token(code)
+    end
+
+    def user_data
+        @user_data ||= Octokit::Client.new( access_token: response )
+        .user
+        .to_h
+        .slice( :login, :avatar_url, :url, :name )
+    end
+
+    def prepare_user
+        @user = if User.exists?(login: user_data[:login])
+            User.find_by(login: user_data[:login])
+        else
+            User.create(user_data.merge(provider: 'github'))
+        end
+    end
 
     attr_reader :code
 end
